a3dcb1accbc7a2b18cb76ebc2093dadbe5492514
Ugo Turner <turner.ugo@gmail.com>
2016-06-11 16:40:09 +0200

    Color in prompt

--- git-to-trello.sh
+++ git-to-trello.sh
@@ -7,7 +7,7 @@ current_project="$2"
 current_branch=$(git branch | grep "*" | sed 's/* //')
 current_user=$(git config user.email)
 
-if [ -z "$(git log origin/master..HEAD)" ];
+if [ -z "$(git log origin/$current_branch..HEAD)" ];
 then
     echo "No commits."
 fi
@@ -15,19 +15,20 @@ fi
 add_card_comment (){
     curl  -s --request POST "https://api.trello.com/1/cards/$1/actions/comments" \
     --data "key=$TRELLO_KEY&token=$TRELLO_TOKEN&text=$2" > /dev/null
+    echo "\033[0;36mAdd comment to Trello card #$1\033[0m"
 }
 
 git log origin/$current_branch..HEAD --reverse --pretty=oneline --abbrev-commit \
 | while read commit;
-do 
+do
     commit_id=$(echo "$commit" | awk -F' ' '{print $1}')
-    commit_full_msg=$(echo "$commit" | awk -F' ' '{print $2}')
+    commit_full_msg=$(echo "$commit" | cut -d" " -f2-)
     raw_card_id=$(echo "$commit_full_msg" | grep -o '#[[:alnum:]]*')
-    if [ ! -z "$raw_card_id" ];
+    if [ ! -z "$raw_card_id" ] && [ "$raw_card_id" != "#" ];
     then
         card_id=$(echo "$raw_card_id" | sed 's/#//g')
         commit_clean_msg=$(echo "$commit_full_msg" | sed 's|'"$raw_card_id "'||g')
-        commit_clean_msg="Commit [#$commit_id]($current_project/commit/$commit_id) - $commit_clean_msg by * $current_user *"
-        add_card_comment "$card_id" "$commit_clean_msg"
+        comment="Commit [#$commit_id]($current_project/commit/$commit_id) - $commit_clean_msg"
+        add_card_comment "$card_id" "$comment"
     fi
 done
